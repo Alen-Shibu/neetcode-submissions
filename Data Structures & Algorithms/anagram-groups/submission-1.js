d05de2c21a671a865e3463164f7f68883aa313c3
Alen Shibu <webwizlab@gmail.com>
2026-06-09 17:22:09 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const sorted = new Map();
+        for(let s of strs){
+            const key = s.split('').sort().join('')
+            if(!sorted.has(key)){
+                sorted.set(key,[])
+            }
+            sorted.get(key).push(s)
+        }
+    return [...sorted.values()];
+    }
+}
