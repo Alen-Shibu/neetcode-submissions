4452401e30f753c757bd0db36912d71f98d4dd54
Alen Shibu <webwizlab@gmail.com>
2026-06-26 14:27:26 +0530

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let l = 0;
+        let r = numbers.length - 1;
+
+        while(l<r){
+            const num = numbers[l] + numbers[r];
+            if(num < target){
+                l++
+            }else if(num > target){
+                r--
+            }else{
+                return [l+1,r+1]
+            }
+        }
+        return;
+    }
+}
