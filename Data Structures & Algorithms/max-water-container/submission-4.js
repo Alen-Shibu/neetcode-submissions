03499a7fa789606aca94605f293352d0a0a7f265
Alen Shibu <webwizlab@gmail.com>
2026-07-04 08:52:01 +0530

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let l = 0;
+        let r = heights.length - 1;
+        let max_area = 0
+
+        while(l<r){
+            const area = (Math.min(heights[l],heights[r])) * (r-l);
+            max_area = Math.max(max_area,area)
+
+            if(heights[l]<heights[r]) l++
+            else r--
+        }
+
+        return max_area
+    }
+}
