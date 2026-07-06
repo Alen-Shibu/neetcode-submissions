5e5c519bc210736b1a61c20469b7b2c3bc46c9ca
Alen Shibu <webwizlab@gmail.com>
2026-07-06 07:26:23 +0530

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let max_area = 0;
+        let l=0;
+        let r = height.length-1
+        let leftMax = height[l]
+        let rightMax = height[r]
+
+        while(l<r){
+            if(leftMax<rightMax){
+                l++;
+                leftMax = Math.max(leftMax,height[l])
+                max_area += leftMax - height[l]
+            }else{
+                r--;
+                rightMax = Math.max(rightMax,height[r])
+                max_area += rightMax - height[r]
+            }
+        }
+
+        return max_area;
+    }
+}
