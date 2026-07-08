dfd252875dbcb0688c332c752b78d297fa1426b6
Alen Shibu <webwizlab@gmail.com>
2026-07-08 05:35:24 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let l = 0;
+        let r = l+1;
+        let max_profit = 0;
+
+        while(r<prices.length){
+            if(prices[l]<prices[r]){
+                const profit = prices[r] - prices[l];
+                max_profit = Math.max(max_profit,profit)
+            }else{
+                l = r;
+            }
+            r++;
+        }
+
+        return max_profit;
+    }
+}
