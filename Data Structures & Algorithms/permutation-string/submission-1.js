cd2d0246e732df4a72ced682e50449ec5e99393c
Alen Shibu <webwizlab@gmail.com>
2026-07-11 13:31:33 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        const len = s1.length;
+        const array = new Array(26).fill(0)
+
+        for(let i=0;i<s1.length;i++){
+            array[s1.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        }
+
+        const actual = array.join('#')
+
+        let l = 0;
+        let arr = new Array(26).fill(0)
+        for(let r=len-1;r<s2.length;r++){
+
+            if(l===0){       
+                for(let i=l;i<=r;i++){
+                    arr[s2.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+                }
+                }else{
+                    arr[s2.charCodeAt(l-1) - 'a'.charCodeAt(0)] --;
+                    arr[s2.charCodeAt(r) - 'a'.charCodeAt(0)] ++;
+                }
+
+
+            const test = arr.join('#')
+
+            if(actual === test) return true;
+            l++;
+        }
+
+        return false;
+    }
+}
