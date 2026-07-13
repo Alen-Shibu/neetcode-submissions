2b7625480d3e51b05944dd024f8aeab9aa17c021
Alen Shibu <webwizlab@gmail.com>
2026-07-14 04:49:15 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let l=0;
+        let max_freq = 0;
+        let max_window = 0;
+        const freq = new Map();
+
+        for(let r=0;r<s.length;r++){
+
+            if(!freq.has(s[r])) freq.set(s[r],0)
+
+            freq.set(s[r],freq.get(s[r])+1)
+            max_freq = Math.max(freq.get(s[r]),max_freq)
+
+            while(r-l+1 - max_freq > k){
+                freq.set(s[l],freq.get(s[l]) - 1);
+                l ++;
+            }
+
+            const window_size = r-l+1;
+            max_window = Math.max(window_size,max_window)
+        }
+
+        return max_window;
+    }
+}
